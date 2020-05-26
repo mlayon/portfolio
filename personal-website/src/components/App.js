e17399d300059223c0fd1656b4c767e4364198b5
Mikhaela Layon <mapl.layon@gmail.com>
2020-05-26 12:44:19 -0700

    creating base app

---
+++ personal-website/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylings/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello from Mikhaela
+    </div>
+  );
+}
+
+export default App;
